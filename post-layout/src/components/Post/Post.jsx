fd0dd2aaa23a6beb1f3a979723b426c0398b7a61
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-08-22 00:17:21 +0500

    fb-layout

--- post-layout/src/components/Post/Post.jsx
+++ post-layout/src/components/Post/Post.jsx
@@ -1,21 +1,43 @@
 import React from 'react'
 import './Post.css'
+import moment from 'moment'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faComment, faShare } from '@fortawesome/free-solid-svg-icons'
+
 
 
 function Post(props) {
   
     return (
       <div className='post'>
-          <img src={props.profilePhoto} alt="profilePhoto" />
 
-          {props.name}
-          {props.date}
+        <div className='postHeader'>
+          <img className='profilePhoto' src={props.profilePhoto} alt="profilePhoto" />
+
+            <div>   
+                {props.name} <br />
+                {moment(props.date).fromNow()}
+            </div>
+        </div>
+
+        <div className='postText'>
           {props.postText}
-          {props.postImage}
+        </div>
 
-          <img src="./" alt="post" />
-          <img src="./" alt="post" />
-          <img src="./" alt="post" />
+        <div className='postImage'>
+          <img src={props.postImage} alt="postImage"  /> 
+        </div>
+
+        <hr />
+        <div className="postFooter">
+           <div> <FontAwesomeIcon icon={faThumbsUp} /> Like </div>
+           <div> <FontAwesomeIcon icon={faComment }/> Comment</div>
+           <div> <FontAwesomeIcon icon={faShare} /> Share</div>
+        </div>
+        <hr />
+
+
+       
 
       </div>
     )
